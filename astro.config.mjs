600fa0f14600d71fd2eb1c87af2e134ee5fca1cc
herjos07 <jos.moy@gmail.com>
2026-08-20 10:05:43 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://herjos.com',
-  base: '/cuentacuentos',
+  site: 'https://herjos.com', // <--- Asegúrate de incluir la URL base aquí
+  base: '/cuentacuentos',     // Tu subruta del proyecto
   integrations: [sitemap()],
 });
